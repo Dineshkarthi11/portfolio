2ecdb9abf89a7e8daf7d022652672364053a39d4
Dineshkarthi11 <166858866+Dineshkarthi11@users.noreply.github.com>
2025-01-27 00:15:58 -0800

    index.js

--- src/index.js
+++ src/index.js
@@ -14,4 +14,48 @@ i need when i click any date in month , week and day , i said when i right click
   do you understand?
 
 
+const renderDayView = () => {
+    const eventsForDay = getEventsForDay(currentDate);
+    return (
+      <div className="day-view-container">
+        <div className="day-header">
+          {formatDate(currentDate, { weekday: 'short', day: 'numeric' })}
+        </div>
+        <div className="day-content">
+          <div className="hours-column">
+            {[...Array(24)].map((_, hour) => (
+              <div key={hour} className="hour-label">{hour}:00</div>
+            ))}
+          </div>
+          <div className="events-column">
+            {[...Array(24)].map((_, hour) => (
+              <div key={hour} className="hour-cell">
+                {eventsForDay
+                  .filter(event => {
+                    const eventHour = event.type === 'long' 
+                      ? event.startDate.getHours() 
+                      : event.date.getHours();
+                    return eventHour === hour;
+                  })
+                  .map(event => (
+                    <div 
+                      key={event.id} 
+                      className={`event ${event.type}`} 
+                      onClick={() => setSelectedEventDetails(event)}
+                    >
+                      <span style={{ marginRight: '4px' }}>
+                        {formatDate(event.date || event.startDate, { hour: '2-digit', minute: '2-digit' })}
+                      </span>
+                      {event.title}
+                    </div>
+                  ))
+                }
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+
+
 
