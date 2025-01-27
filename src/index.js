aeaef64827eb4698dad39605cc7bfecc26a5eee5
Dineshkarthi11 <166858866+Dineshkarthi11@users.noreply.github.com>
2025-01-27 13:47:26 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -9,53 +9,77 @@ root.render(
   </React.StrictMode>
 );
 
-i need when i click any date in month , week and day , i said when i right click or double tab i need to show new event name in tag in that date then if i click on that new event tag i need to show that new event image card i want, i given example of images
-
-  do you understand?
-
-
 const renderDayView = () => {
-    const eventsForDay = getEventsForDay(currentDate);
-    return (
-      <div className="day-view-container">
-        <div className="day-header">
-          {formatDate(currentDate, { weekday: 'short', day: 'numeric' })}
+  const eventsForDay = getEventsForDay(currentDate);
+  return (
+    <div className="day-view-container">
+      <div className="day-header">
+        {formatDate(currentDate, { weekday: 'short', day: 'numeric' })}
+      </div>
+      <div className="day-content">
+        <div className="hours-column">
+          {[...Array(12)].map((_, hour) => (
+            <div key={hour} className="hour-label">
+              {hour === 0 ? 12 : hour}:00 AM
+            </div>
+          ))}
+          {[...Array(12)].map((_, hour) => (
+            <div key={hour + 12} className="hour-label">
+              {hour === 0 ? 12 : hour}:00 PM
+            </div>
+          ))}
         </div>
-        <div className="day-content">
-          <div className="hours-column">
-            {[...Array(24)].map((_, hour) => (
-              <div key={hour} className="hour-label">{hour}:00</div>
-            ))}
-          </div>
-          <div className="events-column">
-            {[...Array(24)].map((_, hour) => (
-              <div key={hour} className="hour-cell">
-                {eventsForDay
-                  .filter(event => {
-                    const eventHour = event.type === 'long' 
-                      ? event.startDate.getHours() 
-                      : event.date.getHours();
-                    return eventHour === hour;
-                  })
-                  .map(event => (
-                    <div 
-                      key={event.id} 
-                      className={`event ${event.type}`} 
-                      onClick={() => setSelectedEventDetails(event)}
-                    >
-                      <span style={{ marginRight: '4px' }}>
-                        {formatDate(event.date || event.startDate, { hour: '2-digit', minute: '2-digit' })}
-                      </span>
-                      {event.title}
-                    </div>
-                  ))
-                }
-              </div>
-            ))}
-          </div>
+        <div className="events-column">
+          {[...Array(12)].map((_, hour) => (
+            <div key={hour} className="hour-cell">
+              {eventsForDay
+                .filter(event => {
+                  const eventHour = event.type === 'long' 
+                    ? event.startDate.getHours() 
+                    : event.date.getHours();
+                  return eventHour === hour;
+                })
+                .map(event => (
+                  <div 
+                    key={event.id} 
+                    className={`event ${event.type}`} 
+                    onClick={() => setSelectedEventDetails(event)}
+                  >
+                    <span style={{ marginRight: '4px' }}>
+                      {formatDate(event.date || event.startDate, { hour: '2-digit', minute: '2-digit', hour12: true })}
+                    </span>
+                    {event.title}
+                  </div>
+                ))
+              }
+            </div>
+          ))}
+          {[...Array(12)].map((_, hour) => (
+            <div key={hour + 12} className="hour-cell">
+              {eventsForDay
+                .filter(event => {
+                  const eventHour = event.type === 'long' 
+                    ? event.startDate.getHours() 
+                    : event.date.getHours();
+                  return eventHour === hour + 12;
+                })
+                .map(event => (
+                  <div 
+                    key={event.id} 
+                    className={`event ${event.type}`} 
+                    onClick={() => setSelectedEventDetails(event)}
+                  >
+                    <span style={{ marginRight: '4px' }}>
+                      {formatDate(event.date || event.startDate, { hour: '2-digit', minute: '2-digit', hour12: true })}
+                    </span>
+                    {event.title}
+                  </div>
+                ))
+              }
+            </div>
+          ))}
         </div>
       </div>
-    );
-
-
-
+    </div>
+  );
+};
