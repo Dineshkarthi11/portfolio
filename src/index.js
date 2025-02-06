4361ea0c1dd43cea5f36ebe0172d36d7900201a5
Dineshkarthi11 <166858866+Dineshkarthi11@users.noreply.github.com>
2025-02-06 15:01:20 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -63,6 +63,67 @@ root.render(
                                     ))
                                 )}
                               </>
+
+
+
+<div className="main-content">
+                      <Form className={key}>
+                        <Accordion defaultActiveKey="0">
+                          <Form.Group className="form-group">
+                            <CustomToggle eventKey="0">
+                              <h4>{sectionlist.name}</h4>
+                              <span>
+                                <IoIosArrowDown />
+                              </span>
+                            </CustomToggle>
+                            <Accordion.Collapse eventKey="0">
+                              <>
+                                {sectionlist.name === "JOB_INFO" ? (
+                                  <JobInfoSection 
+                                    values={jobInfoValues}
+                                    setValues={setJobInfoValues}
+                                    errors={jobInfoErrors}
+                                    setErrors={setJobInfoErrors}
+                                    handleInputChange={handleJobInfoChange}
+                                  />
+                                ) : (
+                                  sectionlist.fields
+                                    ?.slice()
+                                    .sort((a, b) => a.seqno - b.seqno)
+                                    .map((FieldList, index) => (
+                                      <React.Fragment key={index}>
+                                        <DynamicInput
+                                          FieldList={FieldList}
+                                          inputValues={inputValues}
+                                          setInputValues={setInputValues}
+                                          validationErrors={validationErrors}
+                                          setValidationErrors={setValidationErrors}
+                                          showErrors={showErrors}
+                                          setShowErrors={setShowErrors}
+                                          allInputValues={allInputValues}
+                                          setAllInputValues={setAllInputValues}
+                                          selectedTagItem={selectedTagItem}
+                                          setSelectedTagItem={setSelectedTagItem}
+                                          selectedTitleItem={selectedTitleItem}
+                                          setSelectedTitleItem={setSelectedTitleItem}
+                                          setTitleFieldValue={setTitleFieldValue}
+                                          titleFieldValue={titleFieldValue}
+                                          Page={ProjectPageDetails?.page || "AddTask"}
+                                          setArcFilterPopupshow={setArcFilterPopupshow}
+                                          taskInfoPage={taskInfoPage}
+                                          masterDataforPopup={masterDataforPopup}
+                                          disabled={ProjectPageDetails?.disabled || false}
+                                        />
+                                      </React.Fragment>
+                                    ))
+                                )}
+                              </>
+                            </Accordion.Collapse> 
+                          </Form.Group>
+                        </Accordion>
+                      </Form>
+                    </div>
+                                      
                             </Accordion.Collapse> 
                           </Form.Group>
                         </Accordion>
